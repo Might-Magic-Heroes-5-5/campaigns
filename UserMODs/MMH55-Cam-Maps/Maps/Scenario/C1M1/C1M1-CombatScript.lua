14c3ed2fd4249db6814379636735875a736118a0
dredknight <gangstarrr@abv.bg>
2025-08-07 14:13:06 +0300

    feat(C1M1): fixes (#1)

--- UserMODs/MMH55-Cam-Maps/Maps/Scenario/C1M1/C1M1-CombatScript.lua
+++ UserMODs/MMH55-Cam-Maps/Maps/Scenario/C1M1/C1M1-CombatScript.lua
@@ -1,22 +1,20 @@
-once = 0;
 ahero = GetAttackerHero();
 
 function Prepare()
-print( 'prepare');
-SetControlMode( ATTACKER, MODE_AUTO );
-end;
-
+  print( 'prepare');
+  SetControlMode( ATTACKER, MODE_AUTO );
+end
+once = 0;
 function Start()
-print( 'start');
-SetControlMode( ATTACKER, MODE_MANUAL );
-end;
+  print( 'start');
+  SetControlMode( ATTACKER, MODE_MANUAL );
+end
 
 function HeroHint()
 	while 1 do
 		sleep(1);
 		if combatReadyPerson() == ahero then
 			print( combatReadyPerson )
-			--setATB('hero-attacker', 0 );
 			if once == 0 then
 				commandDefend( ahero );
 				print( ahero,' defended' )
@@ -26,29 +24,15 @@ function HeroHint()
 				print( ahero );
 				WaitForTutorialMessageBox();
 				TutorialMessageBox( "c1_m1_t11_hero" );
-				--once = 2;
-			else
 				break;
-			end;
-		end;
-	end;
-end;
+			end
+		end
+	end
+end
 
---sleep( 20 );
-
---print( 'autoplace' );
---SetControlMode( ATTACKER, MODE_AUTO );
---sleep( 1 );
---SetControlMode( ATTACKER, MODE_MANUAL );
---postEvent( 'autoplace_army_force' );
-
---if GetGameVar( "temp.tutorial" ) == "1" then
-	--sleep( 1 );
-	--postEvent( 'confirm_placement' );
-	
 	while combatStarted() == nil do
 		sleep(1);
-	end;
+	end
 
 	--setATB( ahero, 0 );
 	startThread( HeroHint );
@@ -56,18 +40,4 @@ end;
 	WaitForTutorialMessageBox();
 	TutorialMessageBox( "c1_m1_t11_1" );
 	WaitForTutorialMessageBox();
-
-	sleep(5);
-	WaitForTutorialMessageBox();
-	TutorialMessageBox( "c1_m1_t11_2" );
-	WaitForTutorialMessageBox();
-
-	sleep(5);
-	WaitForTutorialMessageBox();
-	TutorialMessageBox( "c1_m1_t11_3" );
-	WaitForTutorialMessageBox();
-
-	sleep(5);
-	WaitForTutorialMessageBox();
-	TutorialMessageBox( "c1_m1_t11_4" );
---end;
+	
