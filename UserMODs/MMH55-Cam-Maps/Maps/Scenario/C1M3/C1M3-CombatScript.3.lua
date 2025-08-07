dd0236b2c3f892f84b92f11e52a396a6368579d6
dredknight <gangstarrr@abv.bg>
2025-08-07 15:03:50 +0300

    feat(C1M3): fixes

--- UserMODs/MMH55-Cam-Maps/Maps/Scenario/C1M3/C1M3-CombatScript.3.lua
+++ UserMODs/MMH55-Cam-Maps/Maps/Scenario/C1M3/C1M3-CombatScript.3.lua
@@ -3,20 +3,20 @@ hired_at_this_visit = 0;
 function WaitForTutorialMessageBox()
 	while IsTutorialMessageBoxOpen() do
 		sleep(1);
-	end;
-end;
+	end
+end
 
 function HeroHired()
-	--if ( GetGameVar( "temp.tutorial", 0 ) == '1' ) then
-	if ( GetGameVar( 'temp.hero_visiting' ) == '1' ) or ( hired_at_this_visit == 1 ) then
-		WaitForTutorialMessageBox();
-		TutorialMessageBox( "c1_m3_herohired" ); -- hero hired
-	end;
-	hired_at_this_visit = 1;
-end;
+  if ( GetGameVar( "temp.tutorial", 0 ) == '1' ) then
+  	if ( GetGameVar( 'temp.hero_visiting' ) == '1' ) or ( hired_at_this_visit == 1 ) then
+  		WaitForTutorialMessageBox();
+  		TutorialMessageBox( "c1_m3_herohired" ); -- hero hired
+  	end
+	  hired_at_this_visit = 1;
+  end
+end
 
---if GetGameVar("temp.tutorial", 0) == "1" then
-	
+if GetGameVar("temp.tutorial", 0) == "1" then
 	SetGameVar("temp.C1M3_Tradeville", GetGameVar("temp.C1M3_Tradeville", 0) + 1 );
 	if GetGameVar("temp.C1M3_Tradeville", 0) == "1" then
 		TutorialMessageBox("c1_m3_t3");
@@ -28,5 +28,5 @@ end;
 		TutorialMessageBox("c1_m3_t9_1");
 	elseif GetGameVar("temp.C1M3_Tradeville", 0) == "5" then
 		TutorialMessageBox( "c1_m4_t11" ); -- hire heroes
-	end;
---end;
+	end
+end
